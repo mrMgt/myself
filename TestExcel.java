c311ce78fe65a849e675dcaad75707ec3a36a642
mrMgt <linhdinhnguyen@gmail.com>
2018-05-15 22:55:02 +0700

    update

--- TestExcel.java
+++ TestExcel.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TestExcel {
 	
@@ -50,7 +52,7 @@ public class TestExcel {
 		
 		item = new ArrayList<>();
 		item.add("3");
-		item.add("link");
+		item.add("link1");
 		item.add("777");
 		listData.add(item);
 		
@@ -76,6 +78,24 @@ public class TestExcel {
 		item.add("3");
 		item.add("link");
 		item.add("133");
+		listData.add(item);
+		
+		item = new ArrayList<>();
+		item.add("4");
+		item.add("href");
+		item.add("133");
+		listData.add(item);
+		
+		item = new ArrayList<>();
+		item.add("5");
+		item.add("auto");
+		item.add("133");
+		listData.add(item);
+		
+		item = new ArrayList<>();
+		item.add("3");
+		item.add("article");
+		item.add("2222");
 		listData.add(item);
 		
 		item = new ArrayList<>();
@@ -123,10 +143,65 @@ public class TestExcel {
 		return listObjectLv1;
 	}
 	
+	public ArrayList<AnObject> parseData111() {
+		int sizeOfData = listData.size();
+		AnObject anObject = null;
+		AnObject anObjectTmp = null;
+		ArrayList<AnObject> listObjectLv1 = new ArrayList<>();
+		ArrayList<AnObject> listObjectCurrent = null;
+		int curerntLevel = 1;
+		int beforeLevel = 1;
+		Map<Integer, AnObject> listParent = new HashMap<>();
+		
+
+		for (int i = 0; i < sizeOfData; i++ ) {
+		    anObject = new AnObject();
+		    anObject.setName(listData.get(i).get(1));
+		    listObjectCurrent = new ArrayList();
+		    anObject.setList(listObjectCurrent);
+		    
+		    curerntLevel = Integer.valueOf(listData.get(i).get(0));
+		    
+		    if (curerntLevel == 1) {
+				anObject.setList(listObjectCurrent);
+		        listObjectLv1.add(anObject );
+		        
+		        listParent.put(curerntLevel, anObject);
+		        
+		    } else if (curerntLevel == beforeLevel) {
+		    	anObject.setList(listObjectCurrent);
+		    	AnObject tempObj = listParent.get(curerntLevel-1);
+		    	tempObj.getList().add(anObject);
+		    	listParent.put(curerntLevel, anObject);
+		    	
+		    } else if (curerntLevel > beforeLevel) {
+		    	
+		    	if (curerntLevel == 4) {
+		    		System.out.println("anObjectTmp   " + anObjectTmp );
+		    	}
+		    	anObject.setList(listObjectCurrent);
+		    	
+		    	anObjectTmp.getList().add(anObject);
+		    	listParent.put(curerntLevel, anObject);
+		    	
+		    } else if (curerntLevel < beforeLevel) {
+		    	int parentLevel = curerntLevel - 1;
+		    	AnObject tempObj = listParent.get(parentLevel);
+		    	tempObj.getList().add(anObject);
+		    	listParent.put(curerntLevel, anObject);
+		    }
+		    anObjectTmp = anObject;
+		    beforeLevel = curerntLevel;
+		}
+		return listObjectLv1;
+	}
+	
 	public void display(ArrayList<AnObject> listObject) {
 		AnObject tempObj = null;
 		AnObject tempObj2 = null;
 		AnObject tempObj3 = null;
+		AnObject tempObj4 = null;
+		AnObject tempObj5 = null;
 		for (int i = 0; i < listObject.size(); i++ ) {
 			tempObj = listObject.get(i);
 			System.err.println("tempObj " + tempObj.getName());
@@ -138,6 +213,14 @@ public class TestExcel {
 						for (int k = 0; k < tempObj2.getList().size(); k++ ) {
 							tempObj3 = tempObj2.getList().get(k);
 							System.err.println("tempObj3 " + tempObj3.getName());
+							for (int l = 0; l < tempObj3.getList().size(); l++ ) {
+								tempObj4 = tempObj3.getList().get(l);
+								System.err.println("tempObj4 " + tempObj4.getName());
+								for (int m = 0; m < tempObj4.getList().size(); m++ ) {
+									tempObj5 = tempObj4.getList().get(m);
+									System.err.println("tempObj5 " + tempObj5.getName());
+								}
+							}
 						}
 					}
 				}
@@ -148,7 +231,7 @@ public class TestExcel {
 	public static void main(String[] args) {
 		TestExcel excel = new TestExcel();
 		excel.init();
-		ArrayList<AnObject> listObject = excel.parseData();
+		ArrayList<AnObject> listObject = excel.parseData111();
 		excel.display(listObject);
 	}
 }
